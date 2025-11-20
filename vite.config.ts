7ea873c085472904d3774eb6cd7e60305bfff347
bleudigital111 <bleu.digital111@gmail.com>
2025-11-19 23:12:58 -0600

    cuarto commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/bleudigital/', // 👈 nombre EXACTO de tu repositorio en GitHub
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-    alias: {
-      '@': path.resolve(__dirname, './src'), // para importar desde src con '@'
-    },
-  },
-  build: {
-    target: 'esnext',
-    outDir: 'build', // 👈 carpeta que gh-pages usará
-  },
-  server: {
-    port: 3000,
-    open: true, // abre el navegador automáticamente
-  },
+  base: '/bleudigital1/'
 })
